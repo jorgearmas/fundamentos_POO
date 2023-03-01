2fb00931085e3f7259f659abd39399a1c55937e7
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-01 00:24:54 -0600

    2 objs instanciated

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,18 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+
+#instanciacion de objeto "car"
+car = Car()
+#asignacion de datos a atributos de clase
+car.license = "AMS985"
+car.driver = "Andres Herrera"
+print(vars(car))
+
+#instanciacion de objeto "car2"
+car2 = Car()
+#asignacion de datos a atributos de clase
+car2.license = "QSQ452"
+car2.driver = "Cookie Cruz"
+print(vars(car2))
