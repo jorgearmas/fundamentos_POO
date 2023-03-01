553d4bc5b9b8d1077751f83d3d3273cd6507b530
Jorge Armas <jorgearmasc0496@gmail.com>
2023-02-28 23:59:05 -0600

    2 objs instanciated - class car

--- Java/main.java
+++ Java/main.java
@@ -2,5 +2,18 @@
 class main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+            /*instanciacion objeto "car"*/
+            Car car = new Car();
+            car.license = "AMQ123";
+            car.driver = "Andres Herrera";
+            car.passenger = 4;
+            car.printDataCar();
+
+            /*instanciacion objeto "car"*/
+            Car car2 = new Car();
+            car2.license = "QWE567";
+            car2.driver = "Cookie Cruz";
+            car2.passenger = 5;
+            car2.printDataCar();
     }
 }
