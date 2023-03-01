1e76b5986f172db4ba11fed2eed3da5e0fbda68c
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-01 01:49:55 -0600

    instances modified with constructor

--- Java/main.java
+++ Java/main.java
@@ -3,18 +3,14 @@ class main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
             /*instanciacion objeto "car"*/
-            Car car = new Car();
+            Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
             /*asignacion de datos de atributos de clase*/
-            car.license = "AMQ123";
-            car.driver = "Andres Herrera";
             car.passenger = 4;
             car.printDataCar();
 
             /*instanciacion objeto "car"*/
-            Car car2 = new Car();
+            Car car2 = new Car("QWE567", new Account("Baxter Cruz", "Cookie Cruz"));
             /*asignacion de datos de atributos de clase*/
-            car2.license = "QWE567";
-            car2.driver = "Cookie Cruz";
             car2.passenger = 5;
             car2.printDataCar();
     }
