183d1a7b9ccdf5e74722992d1f24ed32881a0115
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-01 01:47:25 -0600

    attr driver type changed to Account, constructor built

--- Java/car.java
+++ Java/car.java
@@ -1,11 +1,18 @@
 public class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passenger;
+
+    /*metodo constructor de Car, "this" hace referencia a las
+     * variables globales*/
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
 
     /*metodo que imprime datos de clase*/
     void printDataCar(){
-        System.out.println("License: "+license+" / Driver: "+driver+" / Capacity: "+passenger);
+        System.out.println("License: "+license+" / Driver: "+driver.name+" / Capacity: "+passenger);
     }
 }
