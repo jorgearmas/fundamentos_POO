84c20b6552ad2d223b14c379ab9e49144731c965
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-01 01:46:16 -0600

    Constructor built

--- Java/account.java
+++ Java/account.java
@@ -1,7 +1,14 @@
-public class account {
+public class Account {
     Integer id;
     String name;
     String document;
     String email;
     String password;
+
+    /*metodo constructor de Account, "this" hace referencia a las
+     * variables globales*/
+    public Account(String name, String document){
+        this.name = name;
+        this.document = document;
+    }
 }
