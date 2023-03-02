b18588e1a3bb5cd59b4a6102aafca36998657d74
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-02 12:15:37 -0600

    Constructor creado para Account

--- Python/account.py
+++ Python/account.py
@@ -4,3 +4,12 @@ class account:
     document = str
     email = str
     password = str
+
+    """
+    Metodo Constructor:
+    La palabra self hace referencia a las
+    variables globales. Ingresa como parametro
+    """
+    def __init__(self, name, document):
+        self.name = name
+        self.document = document
