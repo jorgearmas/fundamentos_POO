0deb767b16958006ef9382d34735ecda67a267e9
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-02 10:39:58 -0600

    Refactor class Account - ECMA6

--- JS/Account.js
+++ JS/Account.js
@@ -1,8 +1,10 @@
 //El metodo constructor esta implicito en las vars con asignacion
-function Account(name, document){
-    this.id;
-    this.name = name;
-    this.document = document;
-    this.email;
-    this.password;
+class Account{
+    constructor(name, document){
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
 }
