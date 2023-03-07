958fa6c5ca8b026c83a90e6f77aba14750b0528e
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-06 23:13:31 -0600

    objeto uberX y uberPool creados

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once('Car.php');
+require_once('UberX.php');
+require_once('UberPool.php');
 require_once('Account.php');
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->printDataCar();
+$uberX = new UberX('CVB123', new Account('Andres Herrera', 'AND46'), 'Chevrolet', 'Spark');
+$uberX->printDataCar();
 
-$car2 = new Car("UTF785", new Account("Cookie Dog", "LRD7452"));
-$car2->printDataCar();
-
-?>
+$uberPool = new UberPool('RET745', new Account('Mack Lopez', 'MCL45'), 'Honda', 'Fit');
+$uberPool->printDataCar();
