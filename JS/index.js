0b704788a4328eba40c22dd58b27acffd1d92355
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-07 22:05:27 -0600

    Nuevo objeto UberX creado

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,7 @@
 var car = new Car("AWS456", new Account("Andres Herrera", "LLC6"))
 car.passenger = 4
 car.printDataCar()
+
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA785"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
