a20e07b228844877a148b61329866dc2616e0795
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-09 20:03:45 -0600

    objeto instanciado user

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 from car import Car
 from account import Account
+from user import User
 
 if __name__ == "__main__":
     print("Hola Mundo")
@@ -11,3 +12,7 @@ print(vars(car))
 #instanciacion de objeto "car2"
 car2 = Car("QSQ452", Account("Cookie Cruz","YPF7456"))
 print(vars(car2))
+
+#instanciacion de objeto "user"
+user = User("Jose Garcia", "JGAR968", "jgarcia@sample.net", "pass123")
+print(vars(user))
