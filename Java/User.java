533586f3e6ef2070cdcc4859e9f8a168ae03f05b
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-09 23:07:54 -0600

    clase User hija de clase Account

---
+++ Java/User.java
@@ -0,0 +1,10 @@
+public class User extends Account {
+
+    public User(String name, String document, String email, String password){
+        super(name, document, email, password);
+    }
+
+    public void printDataUser(){
+        System.out.println("Name-> "+name+"\nDocument-> "+document+"\nEmail-> "+email+"\nPassword-> "+password);
+    }
+}
