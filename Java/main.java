67c5bee6fba6351e0a947d62b1ecf08fd2826101
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-10 22:18:48 -0600

    objeto uberX instanciado

--- Java/main.java
+++ Java/main.java
@@ -3,15 +3,15 @@ class main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
             /*instanciacion objeto "car"*/
-            Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123", "aherrera@uberd.com","generic123!"));
+            UberX uberx = new UberX("AMQ123", new Account("Andres Herrera", "AND123", "aherrera@uberd.com","generic123!"),"Chevrolet","Sonic");
             /*asignacion de datos de atributos de clase*/
-            car.passenger = 4;
-            car.printDataCar();
+            uberx.setPassenger(3);
+            uberx.printDataCar();
 
             /*instanciacion objeto "car"*/
             Car car2 = new Car("QWE567", new Account("Baxter Cruz", "Liza Cruz","lcruz@uberd.com","generic123!"));
             /*asignacion de datos de atributos de clase*/
-            car2.passenger = 5;
+            //car2.passenger = 5;
             car2.printDataCar();
 
             User user1 = new User("Luke Mack", "LM785", "lmack@user.net", "pass123!");
